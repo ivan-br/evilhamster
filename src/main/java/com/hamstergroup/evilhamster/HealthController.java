d35fb65ea42a1b05ced09af893d9f39fb2218ae5
ivan.brovenko <ivan.brovenko@imaginelearning.com>
2026-07-18 15:49:25 +0200

    Add root health endpoint

---
+++ src/main/java/com/hamstergroup/evilhamster/HealthController.java
@@ -0,0 +1,15 @@
+package com.hamstergroup.evilhamster;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public Map<String, String> root() {
+        return Map.of("status", "UP");
+    }
+}
